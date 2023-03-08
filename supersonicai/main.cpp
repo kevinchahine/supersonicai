79546f8cf8047eb8473b459b98a8af64941faeb1
Kevin Chahine <kchahine17@gmail.com>
2023-03-08 14:37:41 -0500

    Configured cmake

---
+++ supersonicai/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(int argc, char ** argv) {
+	cout 
+		<< "======================" << endl
+		<< "=== SUPER SONIC AI ===" << endl
+		<< "======================" << endl;
+
+
+	cin.get();
+	return 0;
+}
